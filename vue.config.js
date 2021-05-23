1f36893ddc383ca62504615a1f84145e685b7183
liliang <13366698969@163.com>
2021-05-23 20:24:07 +0800

    测试地址

--- vue.config.js
+++ vue.config.js
@@ -58,7 +58,7 @@ module.exports = {
     proxy: {
       // 配置跨域
       '/api': {
-        target: 'http://g.wetuc.com',
+        target: 'http://wj.ngrok.wjlock.cn',
         // ws: true,
         changOrigin: true, // 开启代理
         secure: true,
